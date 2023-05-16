670dfdf5ee764f31abc86d6d38c3a1546396ca61
JonLayman <133721255+JonLayman@users.noreply.github.com>
2023-05-16 05:57:00 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+
+var myAge = 28;
+
+console.log(myAge);
+
+var earlyYears = 2;
+
+earlyYears = earlyYears *= 10.5;
+
+console.log(earlyYears);
+
+var laterYears = myAge - 2;
+
+laterYears *= 4;
+console.log(laterYears);
+
+var myAgeInDogYears = earlyYears + laterYears;
+
+console.log(myAgeInDogYears);
+
+var myName = 'Jon';
+console.log(myName.toLowerCase());
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
